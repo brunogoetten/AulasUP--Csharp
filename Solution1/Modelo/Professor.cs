b018e1dab2c16096163c0b9ea7c8706d5084fd90
Bruno Augusto Goetten Farias <brunogoetten@gmail.com>
2018-06-23 15:18:36 -0300

    Adicionando classe professor.

---
+++ Solution1/Modelo/Professor.cs
@@ -0,0 +1,14 @@
+﻿namespace Modelos
+{
+    public class Professor
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int Registro { get; set; }
+
+    }
+}
