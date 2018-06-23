a2a1bef08601e7de95efd66343bccc4b898ee13c
Bruno Augusto Goetten Farias <brunogoetten@gmail.com>
2018-06-23 11:53:07 -0300

    Modularização nos métodos ImprimirCadastro e CadastrarAluno

--- Solution1/ConsoleApplication1/Program.cs
+++ Solution1/ConsoleApplication1/Program.cs
@@ -10,6 +10,32 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Aluno a = CadastrarAluno();
+
+            //            a.Nome = "Godofredo";   //set
+            //            a.Matricula = 123; //set
+
+            ImprimirCadastro(a);
+
+            Console.ReadKey();
+        }
+
+        private static void ImprimirCadastro(Aluno a)
+        {
+            Console.WriteLine("Aluno: " + a.Nome);
+            Console.WriteLine("Matricula: " + a.Matricula);
+        }
+
+        private static Aluno CadastrarAluno()
+        {
+            Aluno a = new Aluno();
+
+            Console.Write("Digite o nome do aluno: ");
+            a.Nome = Console.ReadLine();
+
+            Console.Write("Digite o número da matricula: ");
+            a.Matricula = int.Parse(Console.ReadLine());
+            return a;
         }
     }
 }
