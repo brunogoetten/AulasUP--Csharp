453a62d79bee66371162ae6dda0046db4f9b8887
Bruno Augusto Goetten Farias <brunogoetten@gmail.com>
2018-06-23 15:38:13 -0300

    Finalizada Adição do Professor

--- Solution1/ConsoleApplication1/Program.cs
+++ Solution1/ConsoleApplication1/Program.cs
@@ -24,6 +24,20 @@ namespace ConsoleView
                 ImprimirDadosAluno(aluno);
             }
 
+
+            ProfessorController professorController = new ProfessorController();
+            //Cadastrando professor
+            Professor d = CadastrarProfessor();
+            professorController.Inserir(d);
+            Professor e = CadastrarProfessor();
+            professorController.Inserir(e);
+            Professor f = CadastrarProfessor();
+            professorController.Inserir(f);
+            foreach (Professor professor in professorController.ListarTodos())
+            {
+                ImprimirDadosProfessor(professor);
+            }
+
             //ImprimirDadosAluno(a);
             Console.ReadKey();
         }
@@ -40,6 +54,27 @@ namespace ConsoleView
             Console.Write("DIGITE O NOME DA MATRICULA DO ALUNO: ");
             a.Matricula = int.Parse(Console.ReadLine());                     //set
             return a;
+
+            
+
+            //ImprimirDadosProfessor(a);
+            Console.ReadKey();
         }
+        private static void ImprimirDadosProfessor(Professor a)
+        {
+            Console.WriteLine("Professor: " + a.Nome);                      //get
+            Console.WriteLine("Registro: " + a.Registro);             //get
+        }
+        private static Professor CadastrarProfessor()
+        {
+            Professor a = new Professor();
+            Console.Write("DIGITE O NOME DO PROFESSOR: ");
+            a.Nome = Console.ReadLine();                                //set
+            Console.Write("DIGITE O NOME DO REGISTRO DO PROFESSOR: ");
+            a.Registro = int.Parse(Console.ReadLine());                     //set
+            return a;
+
+        }
+
     }
 }
